a67f4aa139362523df37d47699c0f2ec0a4f3415
devops <devopsmca123@gmail.com>
2024-03-13 07:36:40 +0000

    done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('Hello zoo1');
+ res.send('Hello everyone node deploye done');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
